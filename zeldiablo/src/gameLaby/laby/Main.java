0984114306f23b64cf57d59940e0d90db42123f3
Olivier <olivierdhxl@gmail.com>
2024-06-03 11:53:21 +0200

    modif gitignore

--- zeldiablo/src/gameLaby/laby/Main.java
+++ zeldiablo/src/gameLaby/laby/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // charge le labyrinthe
-        Labyrinthe laby = new Labyrinthe("Labyrinthe/labySimple/laby1.txt");
+        Labyrinthe laby = new Labyrinthe("labySimple/laby1.txt");
 
 
         LabyJeu l = new LabyJeu(laby);
