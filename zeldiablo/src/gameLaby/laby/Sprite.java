a20bd23dd3037386ad3178f7d3ff44f05b7cd0f5
tuline <tuline.leveque@gmail.com>
2024-06-07 16:43:28 +0200

    ajout classe Sprite

---
+++ zeldiablo/src/gameLaby/laby/Sprite.java
@@ -0,0 +1,7 @@
+package gameLaby.laby;
+
+public class Sprite {
+    public static final image IMG_HEROS = '.';
+    public static final image IMG_MONSTRE = '.';
+    public static final image IMG_MUR = '.';
+}
