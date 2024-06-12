e7fbe39e7f8ddd37cabc015a3e6727533dbf06c4
Olivier <olivierdhxl@gmail.com>
2024-06-12 15:06:59 +0200

    ajout de fonctionnalités et javadoc Inventaire

--- zeldiablo/src/gameLaby/laby/Inventaire.java
+++ zeldiablo/src/gameLaby/laby/Inventaire.java
@@ -1,16 +1,31 @@
 package gameLaby.laby;
 
 public class Inventaire {
+    /**
+     * liste de string correspondant aux items que le perso possede
+     */
     String[] items;
 
 
+    /**
+     * constructeur
+     */
     public Inventaire() {
         this.items = new String[]{null, null, null};
     }
+
+    /**
+     * getter d'inventaire
+     * @return une liste de strings correspondants items du perso
+     */
     public String[] getInventaire() {
         return this.items;
     }
 
+    /**
+     * permet d'ajouter un item a partir d'un String
+     * @param item est un String correspondant à l'item à ajouter a l'inventaire du perso
+     */
     public void ajouterItem(String item){
         int i = 0;
         while(this.items[i] != null){
