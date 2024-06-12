0c7f9607a067e2c492133b5df2099eb551bfb9a0
Olivier <olivierdhxl@gmail.com>
2024-06-12 11:40:37 +0200

    javadoc Feu

--- zeldiablo/src/gameLaby/laby/Feu.java
+++ zeldiablo/src/gameLaby/laby/Feu.java
@@ -2,7 +2,18 @@ package gameLaby.laby;
 
 public class Feu extends CaseEffet{
 
+    /**
+     * constructeur d'un case Feu
+     * @param x abscisse de la case
+     * @param y ordonnee de la case
+     */
     public Feu(int x, int y) { super(x,y); }
+
+    /**
+     * met le personnage en feu par l'intermediaire de l'activation d'un booleen dans LabyJeu
+     * @param lj est le LabyJeu ou l'on souhaite appliquer cet effet
+     * @param l est le Labyrinthe ou l'on souhaite appliquer cet effet
+     */
     public void executerEffet(LabyJeu lj, Labyrinthe l) {
         lj.setFeu(true);
         System.out.println(l.getPerso().getVie());
